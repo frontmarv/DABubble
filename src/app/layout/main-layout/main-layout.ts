532e3ff2efebc756f3205a1ce87403b19c6710bf
Joshi-hub <joshuaauerbach@web.de>
2026-02-01 09:55:08 +0100

    - Connected MainLayout to AppComponent - Integrated Sidebar into MainLayout - Added basic layout structure (left sidebar, center panel, right panel placeholders)

--- src/app/layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Sidebar } from '../../components/sidebar/sidebar';
+import { ThreadPanel } from '../../components/thread-panel/thread-panel';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [],
+  imports: [CommonModule, Sidebar, ThreadPanel,],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
