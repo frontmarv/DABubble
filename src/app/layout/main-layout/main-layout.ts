5f46d8454e85d41db2c1f09dcf4632dae51d1c33
Christian Klemm <christianklemm1987@gmail.com>
2026-02-02 10:43:53 +0100

    feat: message-composer styling

--- src/app/layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Sidebar } from '../../components/sidebar/sidebar';
 import { ThreadPanel } from '../../components/thread-panel/thread-panel';
+import { MessageComposer } from '../../components/message-composer/message-composer';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [CommonModule, Sidebar, ThreadPanel,],
+  imports: [CommonModule, Sidebar, ThreadPanel,MessageComposer],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
