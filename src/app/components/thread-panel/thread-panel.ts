636d7bbd2a8b26493e0974168eeca22c81c8d5c6
Christian Klemm <christianklemm1987@gmail.com>
2026-02-02 11:27:34 +0100

    feat: positioning of message composer in main-chat and thread

--- src/app/components/thread-panel/thread-panel.ts
+++ src/app/components/thread-panel/thread-panel.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MessageComposer } from '../message-composer/message-composer';
 
 @Component({
   selector: 'app-thread-panel',
-  imports: [],
+  imports: [MessageComposer],
   templateUrl: './thread-panel.html',
   styleUrl: './thread-panel.scss',
 })
