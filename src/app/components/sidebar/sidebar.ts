bddf21c507055698d9c0def32dc31489836ac423
Joshi-hub <joshuaauerbach@web.de>
2026-02-02 11:14:00 +0100

    add collapsible sections and fix list item alignment

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
 export class Sidebar {
+  channelsOpen = true;
+  dmOpen = true;
 
+  toggleChannels() {
+    this.channelsOpen = !this.channelsOpen;
+  }
+
+  toggleDm() {
+    this.dmOpen = !this.dmOpen;
+  }
 }
