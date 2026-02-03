24f89dcf161c284e34fb839b6ef0ffb91cbc3092
marv_len <marvin.lenhart@gmx.net>
2026-02-03 02:17:12 +0100

    moved main layout to app.html/scss/ts chatroom html/css improved

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ChatRoom } from './pages/chat-room/chat-room';
+import { Login } from './pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login', component: Login
+    }
+    ,
+    {
+        path: '', component: ChatRoom,
+    }
+
+];
--- src/app/pages/chat-room/chat-room.ts
+++ src/app/pages/chat-room/chat-room.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Sidebar } from '../../components/sidebar/sidebar';
+import { ThreadPanel } from '../../components/thread-panel/thread-panel';
+import { MessageComposer } from '../../components/message-composer/message-composer';
+import { MessageList } from "../../components/message-list/message-list";
 
 @Component({
   selector: 'app-chat-room',
-  imports: [],
+  imports: [CommonModule, Sidebar, ThreadPanel, MessageComposer, MessageList],
   templateUrl: './chat-room.html',
   styleUrl: './chat-room.scss',
 })
 export class ChatRoom {
+  isSidebarOpen = true;
+  isProfileMenuOpen = false;
 
+  toggleProfileMenu() {
+    this.isProfileMenuOpen = !this.isProfileMenuOpen;
+  }
+
+  toggleSidebar(): void {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 }
