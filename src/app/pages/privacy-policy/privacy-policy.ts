875afeed7745f2d10596481403e54a2938ca40d9
marv_len <marvin.lenhart@gmx.net>
2026-02-04 12:40:01 +0100

    add: responsivness h1-h4 add: privacy policy (responsive)

--- src/app/pages/privacy-policy/privacy-policy.ts
+++ src/app/pages/privacy-policy/privacy-policy.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouterLinkWithHref } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './privacy-policy.html',
   styleUrl: './privacy-policy.scss',
 })
