2d7e14aa8109a464417368ae61f7321075f7ddca
marv_len <marvin.lenhart@gmx.net>
2026-03-01 19:10:33 +0100

    status-img bug fixed; emoji picker in chat inserted;

--- src/app/components/chat/message-composer/message-composer.ts
+++ src/app/components/chat/message-composer/message-composer.ts
@@ -1,14 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { ChatService } from '../../../services/chat.service';
+import { EmojiPickerStateService } from '../../../services/emoji-picker-serivce';
+import { EmojiPicker } from '../../emoji-picker/emoji-picker';
 
 @Component({
   selector: 'app-message-composer',
-  imports: [],
+  imports: [EmojiPicker],
   templateUrl: './message-composer.html',
   styleUrl: './message-composer.scss',
 })
 export class MessageComposer {
 
   chatService = inject(ChatService)
-
+emojiPickerService = inject(EmojiPickerStateService);
 }
--- src/app/components/profile-view/not-logged-in/not-logged-in.ts
+++ src/app/components/profile-view/not-logged-in/not-logged-in.ts
@@ -3,21 +3,30 @@ import { FirebaseService } from '../../../services/firebase.service';
 import { inject } from '@angular/core';
 import { User } from '../../../models/user.class';
 import { DisplayForeignUserService } from '../../../services/display-foreign-user.service';
+import { ChatService } from '../../../services/chat.service';
 
 @Component({
   selector: 'app-not-logged-in',
   imports: [],
   templateUrl: './not-logged-in.html',
-  styleUrl: './not-logged-in.scss',
+  styleUrl: '../profile-view.scss',
 })
 export class NotLoggedIn {
   displayForeignUserService = inject(DisplayForeignUserService);
   firebaseService = inject(FirebaseService);
+  chat = inject(ChatService);
   @Output() close = new EventEmitter<void>();
+  @Output() mobileNavigation = new EventEmitter<void>();
 
   selectedUser: User | null = null;
 
   closeProfile(): void {
     this.close.emit();
   }
+
+  selectDm(user: any) {
+    this.mobileNavigation.emit();
+    this.chat.openChatRoom(user);
+  }
+
 }
